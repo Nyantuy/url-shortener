8979f04fbe8a1a7d84ccb6b253bac8dff73ed053
FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>
2025-05-02 17:54:19 +0700

    fix: delete expired url

--- src/app/[hash]/page.js
+++ src/app/[hash]/page.js
@@ -8,7 +8,23 @@ export default async function RedirectPage({ params }) {
     where: { uid: hash },
   });
 
-  if (record?.url) {
+  if (!record) {
+    redirect('/');
+  }
+
+  if (record.expiredAt && new Date(record.expiredAt) < new Date()) {
+    await prisma.urlInfo.delete({
+      where: { uid: hash },
+    });
+    return (
+      <div className='p-8 text-center'>
+        <h1>Link Expired</h1>
+        <p>The link you are trying to access has expired.</p>
+      </div>
+    );
+  }
+
+  if (record.url) {
     redirect(record.url);
   }
 
